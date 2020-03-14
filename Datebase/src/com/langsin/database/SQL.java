26d5738076c528d6260639567e36f22c8f83bed2
telils <telils@163.com>
2020-03-14 22:36:56 +0800

    开始学习mysql数据库

---
+++ Datebase/src/com/langsin/database/SQL.java
@@ -0,0 +1,13 @@
+package com.langsin.database;
+
+/**
+ * @author 🏹☂࿈秋鹜࿈🏹️
+ * @create 2020/3/14 21:47
+ */
+public class SQL {
+    /**
+     * ### MySQL服务的登录和退出
+     * mysql -h主机名 -P端口号 -u用户名 -p密码
+     * exit或ctrl+C
+     */
+}
